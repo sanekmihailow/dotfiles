d5427ddfacdcc6ba7ad061bb68b5262c3c7a55e2
Aleksandr <sanekmihailow@mail.ru>
2018-02-07 13:44:55 +0300

    install script

--- vim_update.sh
+++ vim_update.sh
@@ -1,43 +1,55 @@
 #!/bin/bash
 
 vim_ver="$(/usr/bin/vim --version | grep "Vi IMproved" | awk '{print $5}' | sed -e 's/\.//g')"
-path="/usr/share/vim/vim$vim_ver/"
-echo "\n $path"
-
+path="/usr/share/vim/vim$vim_ver"
+vimless="/usr/share/vim/vim$vim_ver/macros/less.sh"
 user=$(find /home/ -name "vim_update.sh" |awk -F"/" '{print $3}' |head -n1)
-echo "\n $user"
-
 #check=$(find /home/ -name "vim_update.sh" |awk -F"/" '{$NF=""; print $0}' |sed "s/ /\//g")
 
-mv ./shell_source .shell_source
+mv ./shell_source ./.shell_source &&
 
 if [ -z $vim_ver ]; then
 	echo -e "\033[32m Install vim first \033[0m"
 else
 		#create backup dir
-	mkdir ./backup_dir
+	mkdir -p ./backup_dir/root/ ./backup_dir/user/ &&
 		#move backup dir
-	mv /etc/bash.bashrc ./backup_dir/.bash.bashrc
-	mv /etc/vim/vimrc ./backup_dir/vimrc & mv /home/$user/.vimrc ./backup_dir/.vimrc || mv /etc/vimrc ./backup_dir/vimrc 
-	mv /root/.bashrc ./backup_dir/.bashrc_root
-	mv /home/$user/.bashrc ./bakup_dir/.bashrc_user
-	mv /home/$user/.bash_profile ./backup_dir/.bash_profile_user
-	tar -czf old_vim_bak.tar.gz /usr/share/vim/$vim_ver/colors /usr/share/vim/$vim_ver/syntax /usr/share/vim/$vim_ver/syntax
+	
+	cp /etc/bash.bashrc ./backup_dir/bash.bashrc
+	cp /etc/vim/vimrc ./backup_dir/vimrc || cp /etc/vimrc ./backup_dir/vimrc
+	cp /home/$user/.vimrc ./backup_dir/user/.vimrc
+	cp /home/$user/.bashrc ./backup_dir/user/.bashrc
+	cp /home/$user/.bash_profile ./backup_dir/user/.bash_profile
+	cp /home/$user/.screenrc ./backup_dir/user/.screenrc
+	cp /home/$user/.tmux.conf ./backup_dir/user/.tmux.conf
+	cp /root/.bashrc ./backup_dir/root/.bashrc
+	cp /root/.screenrc ./backup_dir/root/.screenrc
+	cp /root/.tmux.conf ./backup_dir/root/.tmux.conf
+	tar -czf  old_vim_bak.tar.gz /usr/share/vim/vim$vim_ver/colors /usr/share/vim/vim$vim_ver/syntax /usr/share/vim/vim$vim_ver/syntax
 	mv old_vim_bak.tar.gz ./backup_dir
+	chown -R $user ./backup_dir
+
 		#copy dot files from this folder	
 		#---------------------copy root
-	cp ./etc/bash.bashrc /etc/
-	cp .{bashrc_root,screenrc,tmux.conf,vimrc} /root 
-	#cp .bashrc_root /root
-	#cp .screenrc /root
-	#cp .tmux.conf /root
-	#cp .vimrc /root
-	cp -r .shell_source /root
+	cp ./etc/bash.bashrc /etc/bash.bashrc
+	#cp .{bashrc_root,screenrc,tmux.conf,vimrc} /root 
+	cp ./.bashrc_root /root/.bashrc_root
+	cp ./.screenrc /root/.screenrc
+	cp ./.tmux.conf /root/.tmux.conf
+	cp ./.vimrc /root/.vimrc
+	cp -r ./.shell_source/ /root
 		#----------------------copy user
-	cp .{bashrc,bash_profile,screenrc,tmux.conf,vimrc} /home/$user/
-	cp -r .shell_source /home/$user/ 
+	#cp .{bashrc,bash_profile,screenrc,tmux.conf,vimrc} /home/$user/
+	cp  ./.bashrc /home/$user/.bashrc
+	cp ./.bash_profile /home/$user/.bash_profile
+	cp ./.screenrc /home/$user/.screenrc
+	cp ./.tmux.conf /home/$user/.tmux.conf
+	cp ./.vimrc /home/$user/.vimrc
+	cp -r ./.shell_source/ /home/$user/ 
 		# COPY vim files
 	cp ./usr/share/vim/vimXX/colors/* $path/colors/
 	cp ./usr/share/vim/vimXX/plugin/* $path/plugin/
-	cp ./usr/share/vim/vimXX/syntax/* $path/syntax/	
+	cp ./usr/share/vim/vimXX/syntax/* $path/syntax/
+	ln -ns $vimless /usr/bin/vless
 fi
+exit 0
