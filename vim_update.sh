2db418a384d897f69d2cdc00529116d67e61ef64
Alexandr <sanekmihailow@mail.ru>
2018-06-08 20:49:01 +0300

    Update vim_update.sh

--- vim_update.sh
+++ vim_update.sh
@@ -3,7 +3,7 @@
 vim_ver="$(/usr/bin/vim --version | grep "Vi IMproved" | awk '{print $5}' | sed -e 's/\.//g')"
 path="/usr/share/vim/vim$vim_ver"
 vimless="/usr/share/vim/vim$vim_ver/macros/less.sh"
-user=$(find /home/ -name "vim_update.sh" |awk -F"/" '{print $3}' |head -n1)
+user=$(find /home/ -name "vim_update.sh" 2>/dev/null |awk -F"/" '{print $3}' |head -n1)
 #check=$(find /home/ -name "vim_update.sh" |awk -F"/" '{$NF=""; print $0}' |sed "s/ /\//g")
 
 mv ./shell_source ./.shell_source &&
@@ -32,14 +32,17 @@ else
 		#copy dot files from this folder	
 		#---------------------copy root
 	cp ./etc/bash.bashrc /etc/bash.bashrc
-	#cp .{bashrc_root,screenrc,tmux.conf,vimrc} /root 
-	cp ./.bashrc_root /root/.bashrc_root
-	cp ./.screenrc /root/.screenrc
-	cp ./.tmux.conf /root/.tmux.conf
-	cp ./.vimrc /root/.vimrc
-	cp -r ./.shell_source/ /root
+	cp  ./.bashrc /root/.bashrc
+	echo 'homeuser=$(find /home/ -name ".bash_history" 2>/dev/null |awk -F"/" '{print \$3}' |head -n1)' > /root/.bashrc
+	echo 'source /home/"$homeuser"/.bashrc' >> /root/.bashrc
+#cp .{bashrc_root,screenrc,tmux.conf,vimrc} /root 
+#cp ./.bashrc_root /root/.bashrc_root
+#cp ./.screenrc /root/.screenrc
+#cp ./.tmux.conf /root/.tmux.conf
+#cp ./.vimrc /root/.vimrc
+#cp -r ./.shell_source/ /root
 		#----------------------copy user
-	#cp .{bashrc,bash_profile,screenrc,tmux.conf,vimrc} /home/$user/
+#cp .{bashrc,bash_profile,screenrc,tmux.conf,vimrc} /home/$user/
 	cp  ./.bashrc /home/$user/.bashrc
 	cp ./.bash_profile /home/$user/.bash_profile
 	cp ./.screenrc /home/$user/.screenrc
