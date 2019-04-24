3ba2f21d31e9ed7a3762c93ec35897876e7dff27
Alexandr <sanekmihailow@mail.ru>
2019-04-24 16:04:16 +0300

    simpled

--- dot_update.sh
+++ dot_update.sh
@@ -10,11 +10,12 @@ echo -e "\n \033[0;32m please enter the username in your home directory (root or
 read current_user &&
 user="$(who | cut -d' ' -f1 | grep "$current_user" |head -n1)"
 
-if [ "$user" = "$current_user" ];then
+if [ "$user" = "$current_user" ]; then
         echo "$user found"
 else    echo "$current_user not found" && exit 1
 fi
 
+other_home="bash.bashrc grc.conf"
 root_root=".bashrc .screenrc .tmux.conf .vimrc .source-root .start-screen"
 home_root=".bashrc .screenrc .tmux.conf .vimrc"
 home_user=".bashrc .screenrc .tmux.conf .vimrc .source-home .start-screen"
@@ -26,44 +27,55 @@ mv ./shell_source ./.shell_source;
 		exit 1
 		
 	else
-			#create backup dir
+		#---- create backup dir
 		mkdir -p ./backup_dir/root/ ./backup_dir/user/ &&
-			#move backup dir
-	
-		cp /etc/bash.bashrc ./backup_dir/bash.bashrc
-		cp /etc/grc.conf ./backup_dir/grc.conf
+		#---- move backup dir
+
+        	for h in $home_user; do
+            		cp /home/${user}/${h} ./backup_dir/user/
+		done
+
+        	for r in $home_root; do
+            		cp /root/${r} ./backup_dir/root/
+       		done
+
+        	for e in $other_home; do
+            		cp /etc/${e} ./backup_dir/
+	    	done
+		
 		cp /etc/vim/vimrc ./backup_dir/vimrc || cp /etc/vimrc ./backup_dir/vimrc
-		cp /home/$user/.vimrc ./backup_dir/user/.vimrc
-		cp /home/$user/.bashrc ./backup_dir/user/.bashrc
-		cp /home/$user/.bash_profile ./backup_dir/user/.bash_profile
-		cp /home/$user/.screenrc ./backup_dir/user/.screenrc
-		cp /home/$user/.tmux.conf ./backup_dir/user/.tmux.conf
-		cp /root/.bashrc ./backup_dir/root/.bashrc
-		cp /root/.screenrc ./backup_dir/root/.screenrc
-		cp /root/.tmux.conf ./backup_dir/root/.tmux.conf
-		tar -czf  old_vim_bak.tar.gz /usr/share/vim/vim$vim_ver/colors /usr/share/vim/vim$vim_ver/syntax /usr/share/vim/vim$vim_ver/syntax
-		tar -czf  old_grc_bak.tar.gz /usr/share/grc
-		mv old_vim_bak.tar.gz ./backup_dir
-		chown -R $user ./backup_dir &&
 		cp ./etc/bash.bashrc /etc/bash.bashrc
+		
+		vim_tar="./backup_dir/vim_tar/"
+		vim_fpath="/usr/share/vim/vim${vim_ver}"
+		grc_fpath="/usr/share/grc"
+        	
+		tar -czf  old_vim_bak.tar.gz "$vim_fpath"/colors "$vim_fpath"/syntax
+		tar -czf  old_grc_bak.tar.gz "$grc_fpath"
+		mv old_vim_bak.tar.gz ./backup_dir
+        	mv old_grc_bak.tar.gz ./backup_dir
+        	mkdir ./backup_dir/{vim_tar,grc_tar}
+        	cp -r "$vim_fpath"/colors "$vim_tar"
+        	cp -r "$vim_fpath"/syntax "$vim_tar"
+        	cp -r "$grc_fpath" ./backup_dir/grc_tar
+		chown -R $user ./backup_dir &&
 	fi
 	
-	
 	if [ "$current_user" = "root" ]; then
-			#---------------------copy root_only
+		#---- copy root_only
 		for i in $root_root; do
 			cp ./$i /root
 		done
 			cp ./.bashrc_only_root /root/.bashrc
 			cp -r ./.shell_source /root
 	else
-			#---------------------copy root
+		#---- copy root
 		for a in $home_root; do
 			cp ./$a /root
 		done
 			cp ./bashrc_root /root/.bashrc
 			
-			#----------------------copy user
+		#---- copy user
 		for b in $home_user; do
 			cp ./$b /home/$user/
 			chmod g=rX,o=rX /home/$user/$b
@@ -79,39 +91,36 @@ mv ./shell_source ./.shell_source;
 		exit 1
 		
 	else						
-			# COPY vim files
+		#---- COPY vim files
 		cp ./usr/share/vim/vimXX/colors/* $path/colors/ || echo -e "not vim colors"
 		cp ./usr/share/vim/vimXX/plugin/* $path/plugin/ || echo -e "not vim plugins"
 		cp ./usr/share/vim/vimXX/syntax/* $path/syntax/ || echo -e "not vim syntax"
 		ln -ns $vimless /usr/bin/vless
 			
-			# COPY files in /usr/bin
-			
-		#chmod a+rx ./usr/bin/{colorex,vimcat,tmux-sessions} &&
+		#---- COPY files in /usr/bin	
+		#Er chmod a+rx ./usr/bin/{colorex,vimcat,tmux-sessions} &&
 		chmod a+rx ./usr/bin/colorex && echo "executable colorex \n"
 		chmod a+rx ./usr/bin/vimcat && echo "executable vimcat \n"
 		
-			if [ -z $(which vimcat) ]; then
-				cp ./usr/bin/vimcat /usr/local/bin/
-			else
-				echo 'vimcat exist     '
-			fi
+		if [ -z $(which vimcat) ]; then
+			cp ./usr/bin/vimcat /usr/local/bin/; else
+			echo 'vimcat exist     '
+		fi
 			
-			if [ -z $(which colorex) ]; then
-				cp ./usr/bin/colorex /usr/local/bin/
-			else
-				echo 'colorex exist    '
-			fi
+		if [ -z $(which colorex) ]; then
+			cp ./usr/bin/colorex /usr/local/bin/; else
+			echo 'colorex exist    '
+		fi
 			
-			if [ -z $(which grc) ]; then
-				cp ./usr/bin/grc /usr/local/bin/
-				cp ./usr/bin/grcat /usr/local/bin/
-				cp -r ./usr/share/grc/ /usr/local/share/
-				cp ./etc/grc.conf /etc/grc.conf
-			else
-				cp -r ./usr/share/grc/* /usr/share/grc/
-				cp ./etc/grc.conf /etc/grc.conf
-			fi
+		if [ -z $(which grc) ]; then
+			cp ./usr/bin/grc /usr/local/bin/
+			cp ./usr/bin/grcat /usr/local/bin/
+			cp -r ./usr/share/grc/ /usr/local/share/
+			cp ./etc/grc.conf /etc/grc.conf
+		else
+			cp -r ./usr/share/grc/* /usr/share/grc/
+			cp ./etc/grc.conf /etc/grc.conf
+		fi
 		echo -e "\n \033[1;32m Congratulations , you installed vim-bashrc \n \033[0m"
 	
 	fi
