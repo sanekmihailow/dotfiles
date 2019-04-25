ddaa2b09c45061d724383a88fb6160c9d5931536
Alexandr <sanekmihailow@mail.ru>
2019-04-25 16:47:20 +0300

    fix remove vim and grc files

--- dot_disupdate.sh
+++ dot_disupdate.sh
@@ -8,7 +8,7 @@ other_dor="bash.bashrc grc.conf"
 echo -e "\n \033[0;32m Are you shure? It's script delete dot files in /etc/ /home/user and /root and restore old dot files.\nAnswer "Yes" or "No" \n \033[0m"
 read answer &&
 if [ "$answer" = 'Yes' ]; then
-    #---- Remove current
+    #C---- Remove current
     echo -e "\nRemoved current dot files"
     for i in $user_dot; do
         rm /home/${user}/${i}
@@ -18,22 +18,39 @@ if [ "$answer" = 'Yes' ]; then
     for a in $other_dot; do
         rm /etc/${a}
     done
-
+    
+    colors="/usr/share/vim/vim${vim_ver}/colors"
+    plugin="/usr/share/vim/vim${vim_ver}/plugin"
+    syntax="/usr/share/vim/vim${vim_ver}/syntax"
+    inside_colors="fairyloss.vim noctu.vim sm.vim sm.vim.tar wombat.vim xoria256.vim"
+    inside_plugin="head.vim vim-translate.vim"
+    inside_syntax="httplog.vim log.vim nginx.vim sm.vim"
     rm /etc/vim/vimrc || rm /etc/vimrc
-    rm -rf /usr/share/vim/vim$vim_ver/colors
-    rm -rf /usr/share/vim/vim$vim_ver/syntax
-    rm -rf /usr/share/grc
+    
+    for col in $inside_colors; do
+        rm "$colors"/"$col"
+    done
+    
+    for plug in $inside_plugin; do
+        rm "$plugin"/"$plug"
+    done    
+    
+    for syn in $inside_syntax; do
+        rm "$syntax"/"$syn"
+    done
+    
+    rm -rf /usr/share/grc/*
 
-    #---- Copy old
+    #C---- Copy old
     echo -e "\nCopied old dot files"
     for b in $other_dot; do
         cp ./backup_dir/${b} /etc/
     done
 
     cp ./backup_dir/vimrc /etc/vimrc || cp ./bachkup_dir/vimrc /etc/vim/vimrc
-    cp -r ./backup_dir/vim_tar/colors /usr/share/vim/vim$vim_ver/
-    cp -r ./backup_dir/vim_tar/syntax /usr/share/vim/vim$vim_ver/
-    cp -r ./backup_dir/grc_tar/grc /usr/share/
+    cp -rf ./backup_dir/vim_tar/colors /usr/share/vim/vim$vim_ver/
+    cp -rf ./backup_dir/vim_tar/syntax /usr/share/vim/vim$vim_ver/
+    cp -rf ./backup_dir/grc_tar/grc /usr/share/
 
     for c in $user_dot; do
         cp ./backup_dir/user/${c} /home/${user}/
