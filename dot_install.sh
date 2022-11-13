b5478129fd43927232bce5e82ea64c23d062528b
Alexandr <sanekmihailow@gmail.com>
2022-11-13 15:53:48 +0300

    Update dot_install.sh

--- dot_install.sh
+++ dot_install.sh
@@ -46,12 +46,17 @@ else
 		
     cp -f /etc/vim/vimrc ./backup_dir/vimrc || cp /etc/vimrc ./backup_dir/vimrc
     cp -f ./etc/bash.bashrc /etc/bash.bashrc
+
+    if [ -z $(which mc) ]; then
+        cp -r $HOME/.config/mc ./backup_dir/
+    fi    
 		
     vim_tar="./backup_dir/vim_tar/"
     grc_tar="./backup_dir/grc_tar/"
     vim_fpath="/usr/share/vim/vim${vim_ver}"
     grc_fpath="/usr/share/grc"
-        	
+    mc_fpath="~/.config/mc"
+    
     tar -czf  ./backup_dir/old_vim_bak.tar.gz "$vim_fpath"/colors "$vim_fpath"/syntax
     tar -czf  ./backup_dir/old_grc_bak.tar.gz "$grc_fpath"
     mv old_vim_bak.tar.gz ./backup_dir
@@ -67,8 +72,8 @@ else
     if [ "$current_user" = "root" ]; then
         #C---- copy root_only
         for i in $root_root; do
-	    cp ./$i /root
-	done
+	        cp ./$i /root
+	    done
 	    cp ./.bashrc_only_root /root/.bashrc
 	    cp -r ./.shell_source /root 
     else
@@ -83,7 +88,7 @@ else
             cp ./$b /home/$user/
             chmod g=rX,o=rX /home/$user/$b
             chown root:$user /home/$user/$b
-	done
+	    done
         cp -r ./.shell_source /home/$user
         chown -R root:$user /home/$user/.shell_source
         chmod -R g=rX,o=rX /home/$user/.shell_source
@@ -91,59 +96,63 @@ else
     echo -e "\n\033[32m Dot files changed \033[0m"
     
     if [ -z $vim_ver ]; then
-         echo -e "\033[31m Install vim first \033[0m" &&
-         exit 1	
+        echo -e "\033[31m Install vim first \033[0m" &&
+        exit 1	
     else						
-         #---- COPY vim files
-         cp -f ./usr/share/vim/vimXX/colors/* $path/colors/ || echo -e "not vim colors"
-         cp -f ./usr/share/vim/vimXX/plugin/* $path/plugin/ || echo -e "not vim plugins"
-         cp -f ./usr/share/vim/vimXX/syntax/* $path/syntax/ || echo -e "not vim syntax"
-         ln -ns $vimless /usr/bin/vless
+        #---- COPY vim files
+        cp -f ./usr/share/vim/vimXX/colors/* $path/colors/ || echo -e "not vim colors"
+        cp -f ./usr/share/vim/vimXX/plugin/* $path/plugin/ || echo -e "not vim plugins"
+        cp -f ./usr/share/vim/vimXX/syntax/* $path/syntax/ || echo -e "not vim syntax"
+        ln -ns $vimless /usr/bin/vless
 			
-         #---- COPY files in /usr/bin	
-         #Er chmod a+rx ./usr/bin/{colorex,vimcat,tmux-sessions} &&
-	 list_ex="colorex vimcat grc grcat tmux-sessions"
-	 
-	 for list in $list_ex; do
-	     chmod a+rx ./usr/bin/${list} 
-	     #&& echo -e "executable ${list}"
-	 done
- 	
-         if [ -z $(which vimcat) ]; then
-             cp ./usr/bin/vimcat /usr/local/bin/
-             echo -e "\n\033[32m Copied vimcat \033[0m"; else
-             echo 'vimcat exist     '
-         fi
+        #---- COPY files in /usr/bin	
+        #Er chmod a+rx ./usr/bin/{colorex,vimcat,tmux-sessions} &&
+	    list_ex="colorex vimcat grc grcat tmux-sessions"
+	
+	    for list in $list_ex; do
+	        chmod a+rx ./usr/bin/${list} 
+	        #&& echo -e "executable ${list}"
+	    done
+
+        if [ -z $(which vimcat) ]; then
+            cp ./usr/bin/vimcat /usr/local/bin/
+            echo -e "\n\033[32m Copied vimcat \033[0m"; else
+            echo 'vimcat exist     '
+        fi
 			
-         if [ -z $(which colorex) ]; then
-             cp ./usr/bin/colorex /usr/local/bin/
-             echo -e "\n\033[32m Copied colorex \033[0m"; else
-             echo 'colorex exist    '
-         fi
+        if [ -z $(which colorex) ]; then
+            cp ./usr/bin/colorex /usr/local/bin/
+            echo -e "\n\033[32m Copied colorex \033[0m"; else
+            echo 'colorex exist    '
+        fi
+
+        if [ -z $(which mc) ]; then
+            cp -r /.config/mc ./config/
+        fi
 			
-         if [ -z $(which grc) ]; then
-             cp ./usr/bin/grc /usr/local/bin/
-             cp ./usr/bin/grcat /usr/local/bin/
-             cp -r ./usr/share/grc /usr/local/share/
-             cp ./etc/grc.conf /etc/grc.conf
-             echo -e "\n\033[32m Create grc /usr/loca/bin \033[0m"
-         else
-	     if [[ $(which grc) == '/usr/bin/grc' ]]; then
-             cp -r ./usr/share/grc/* /usr/share/grc/
-             cp ./etc/grc.conf /etc/grc.conf
-             echo -e "\n\033[32m Copied files grc /usr/share/grc \033[0m"
-	     fi
-	     echo '2' > /dev/null
-         fi
-	 
-	 #COPY FONTS
-	 if [ -d /usr/local/share/fonts ]; then
-	 	cp -r ./usr/local/share/fonts/* /usr/local/share/fonts/
-	  else 
-		mkdir -p /usr/local/share/fonts &&
-		cp -r ./usrl/local/share/fonts/* /usr/local/share/fonts/
-		fc-cache -f -v
-	 fi	
+        if [ -z $(which grc) ]; then
+            cp ./usr/bin/grc /usr/local/bin/
+            cp ./usr/bin/grcat /usr/local/bin/
+            cp -r ./usr/share/grc /usr/local/share/
+            cp ./etc/grc.conf /etc/grc.conf
+            echo -e "\n\033[32m Create grc /usr/loca/bin \033[0m"
+        else
+	        if [[ $(which grc) == '/usr/bin/grc' ]]; then
+                cp -r ./usr/share/grc/* /usr/share/grc/
+                cp ./etc/grc.conf /etc/grc.conf
+                echo -e "\n\033[32m Copied files grc /usr/share/grc \033[0m"
+	        fi
+	    echo '2' > /dev/null
+        fi
+	
+	#COPY FONTS
+	    if [ -d /usr/local/share/fonts ]; then
+	        cp -r ./usr/local/share/fonts/* /usr/local/share/fonts/
+	    else 
+		    mkdir -p /usr/local/share/fonts &&
+		    cp -r ./usrl/local/share/fonts/* /usr/local/share/fonts/
+		    fc-cache -f -v
+	    fi	
 		
 		
     echo -e "\n \033[1;32m Congratulations , you updated dot_files \n \033[0m"
