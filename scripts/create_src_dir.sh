2ba0405cacf89aeb9d8e244ca3c31a7ed476cc38
sanekmihialow <sanekmihialow@gmail.com>
2023-08-15 18:29:15 +0000

    add new link in /etc and /var/log

--- scripts/create_src_dir.sh
+++ scripts/create_src_dir.sh
@@ -1,36 +1,54 @@
 #!/usr/bin/env bash
 home_path=$(whoami)
 local_path="/usr/local/MY_LOCAL"
-mkdir -p "$local_path"/LOCAL &
-mkdir -p "$local_path"/InstalleD
-mkdir -p "$local_path"/INSTALLED_path         || echo 'not create Installed path'
-mkdir -p "$local_path"/INSTALLED_path/GPG     || echo 'not crate Installed_path/GPG'
-mkdir -p "$local_path"/INSTALLED_path/Package/{dpkg,apt,yaourt,yum,snap,dnf,pacman} || echo 'not crate Installed_path/Package'
-mkdir -p "$local_path"/INSTALLED_path/Src     || echo 'not crate Installed_path/Src'
-mkdir -p "$local_path"/INSTALLED_path/OSgrade || echo 'not crate Installed_path/Src'
-mkdir -p "$local_path"/INSTALLED_path/OSup    || echo 'not crate Installed_path/Src'
-mkdir -p "$local_path"/INSTALL/src            || echo 'not crate Install/src'
-mkdir -p "$local_path"/INSTALL/builded        || echo 'not crate Install/builded'
-mkdir -p "$local_path"/INSTALL/package/{dpkg,apt,yaourt,yum,snap,dnf,pacman}        || echo 'not crate Install/package'
-mkdir -p "$local_path"/INSTALL/repo           || echo 'not crate Install/repo'
-mkdir -p "$local_path"/INSTALL/script         || echo 'not crate Install/script'
-mkdir -p "$local_path"/INSTALL/docker         || echo 'not crate Install/docker'
-mkdir -p "$local_path"/BACKUP/conf            || echo 'not crate Backup/conf'
-mkdir -p "$local_path"/BACKUP/file            || echo 'not crate Backup/file'
-mkdir -p "$local_path"/BACKUP/lib             || echo 'not crate Backup/lib'
-mkdir -p "$local_path"/BACKUP/script          || echo 'not crate Backup/script'
-mkdir -p "$local_path"/SCRIPTS/RESULT-scripts || echo 'not crate Result-script'
-mkdir -p "$local_path"/SCRIPTS/Script         || echo 'not crate script'
 
-ln -s "$local_path"/LOCAL /usr/
-ln -s "$local_path"/INSTALLED_path /usr/
-ln -s "$local_path"/INSTALL /usr/
-ln -s "$local_path"/BACKUP /usr/
-ln -s "$local_path"/SCRIPTS /usr/
+list_dir='
+LOCAL
+InstalleD
+INSTALLED_path
+INSTALLED_path/GPG
+INSTALLED_path/Src
+INSTALLED_path/OSgrade
+INSTALLED_path/OSup
+INSTALL/src
+INSTALL/builded
+INSTALL/repo
+INSTALL/script
+INSTALL/docker
+BACKUP/conf   
+BACKUP/file   
+BACKUP/lib    
+BACKUP/script 
+SCRIPTS/RESULT-scripts
+SCRIPTS/Script
+'
+
+list_dirs='
+LOCAL
+INSTALLED_path
+INSTALL
+BACKUP
+SCRIPTS
+'
+
+for directory in $list_dir; do
+    mkdir -p "$local_path"/"${directory}" || echo "not create ${directory}"
+done
+
+mkdir -p "$local_path"/INSTALLED_path/Package/{dpkg,apt,yaourt,yum,snap,dnf,pacman} || echo 'not crate Installed_path/Package'
+mkdir -p "$local_path"/INSTALL/package/{dpkg,apt,yaourt,yum,snap,dnf,pacman}        || echo 'not crate Install/package'
+
+
+for dir in $list_dirs; do
+    ln -s "$local_path"/"${dir}" /usr/
+done
+
+ln -s "$local_path"/SCRIPTS/Script /etc/Scripts
+ln -s "$local_path"/SCRIPTS/RESULT-scripts /var/log/Result-scripts
 
 if [[ "$home_path" = 'root' ]]; then
-    mkdir -p /root/HOME_root/{Script,Result-scripts,Install,Local}; else
-    mkdir -p /home/"$home_path"/HOME/{Script,Result-scripts,Install,Local}
+    mkdir -p /root/HOME_root/{_Scripts,_Result,_Install}; else
+    mkdir -p /home/"$home_path"/HOME/{_Scripts,_Result,_Install}
 fi    
 
 echo 'Install , Istall_path, scripts and Backup dirs created'
