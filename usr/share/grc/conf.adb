c7559fe8588ce5af50e24627cd4bbb0413268ed2
sanekmihailow <sanekmihailow@gmail.com>
2024-02-11 17:01:39 +0300

    add new grc conf files

---
+++ usr/share/grc/conf.adb
@@ -0,0 +1,75 @@
+# adb
+regexp=·''·..·''·..·''·..·''·..·''·..·''·..·''·..·''·..·''·..·''·..·''·..·''·..·
+colours=on_yellow red
+count=more
+======
+regexp=.*starting.*at.*
+colours=on_blue yellow
+count=more
+======
+regexp=.*endinging.*at.*
+colours=on_blue yellow
+count=more
+======
+regexp=.*\[.*%\]
+colours=on_black green bold
+count=more
+======
+regexp=.*(?i)SUCCESS.*
+colours=on_green black
+count=more
+======
+regexp=.*(?i)ERROR.*
+colours=on_red black beep
+count=more
+======
+regexp=.*(?i)FAILED.*
+colours=on_red black beep
+count=more
+======
+regexp=.*(?i)INVALID.*
+colours=on_red black beep
+count=more
+======
+regexp=.*(?i)DEBUG.*
+colours=on_cyan black
+count=more
+======
+regexp=(?i)OKAY
+colours=on_black green bold
+count=more
+======
+regexp=(?i)writing '.*'...
+colours=on_black green bold
+count=more
+======
+regexp=.*target.*max======download======size.*
+skip=yes
+======
+regexp=WHITE_ON_BLACK
+colours=on_black white
+count=more
+======
+regexp=RED_ON_GREEN
+colours=on_green red
+count=more
+======
+regexp=BLUE_ON_YELLOW
+colours=on_yellow blue
+count=more
+======
+regexp=YELLOW_ON_BLUE
+colours=on_blue yellow
+count=more
+======
+regexp=BLACK_ON_MAGENTA
+colours=on_magenta black
+count=more
+======
+regexp=BLACK_ON_CYAN
+colours=on_cyan black
+count=more
+======
+regexp=BLACK_ON_WHITE
+colours=on_white black
+count=more
