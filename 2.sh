ac67d3ebe7626ea1032a907a95c4cde837177347
sanekmihailow <sanekmihailow@gmail.com>
2025-01-11 12:09:26 +0300

    add kubectl alias

--- 2.sh
+++
@@ -1,6 +0,0 @@
-find / -type d  \( -path '/proc' -o -path '/dev' -o -path '/sys' \
-                   -o -path '/var/log/*' -o -path '/var/lib/docker/*' \
-                   -o -path '/mnt' \) \
-                -prune -o -type f -cmin -60 \
-                -printf '%Td-%Tm-%TY %TH:%TM %s\t %p %m %u:%g (%c) [%i] \n' \
-        |awk '{ $3 = substr(($3/1024), 1, 4) "k""\t"; $4=$4 "\t " ; print }';
